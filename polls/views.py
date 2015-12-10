d4230d88cfd6e701f594520cc1b58ba8da9940b5
sttr110 <mydogs@live.jp>
2015-12-10 16:22:09 +0000

    create vote method

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,34 @@
 #from django.shortcuts import render
 #from django.template import Context, loader
 #from django.http import HttpResponse
-from polls.models import Poll
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from polls.models import Choice, Poll
+from django.template import RequestContext
 
 def index(request):
   polls = Poll.objects.all()
   return render_to_response('polls/index.html', {'polls': polls})
 
 def detail(request, poll_id):
+  p = get_object_or_404(Poll, pk=poll_id)
+  return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
+
+def vote(request, poll_id):
+  p = get_object_or_404(Poll, pk=poll_id)
+  try:
+    selected_choice=p.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    return render_to_response('polls/detail.html', {
+        'poll': p,
+        'error_message': "you not selected",
+    }, context_instance=RequestContext(request))
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+
+def results(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p})
-
-
-  #try:
-  #  p = Poll.objects.get(pk=poll_id)
-  #except Poll.DoesNotExist:
-  #  raise Http404
-
-
-  #t = loader.get_template('polls/index.html')
-  #c = Context({
-  #  'polls': polls, 
-  #})
-  #return HttpResponse(t.render(c))
-
+    return render_to_response('polls/results.html', {'poll': p})
